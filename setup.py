bca44f0c5d7eee87c7a522fb61d5a41d63ae42f6
anupamjha977 <anupamjha223@gmail.com>
2023-05-04 00:38:51 +0530

    updated requirement.txt

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+   name="sensor",
+   version="0.0.1",
+   author="anupam",
+   author_email="anupamjha223@gmail.com",
+   packages=find_packages(),
+   install_requires=get_requirements(),
+
+)
